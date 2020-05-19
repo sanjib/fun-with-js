4c9788f750dd603db10d3ab3703566437649fc26
Sanjib Ahmad <sanjib@sanjib.org>
2020-05-20 02:08:32 +0800

    Add scroll magic 101 duration & trigger hook

--- scrollmagic/s101/js/main.js
+++ scrollmagic/s101/js/main.js
@@ -5,14 +5,18 @@ $(document).ready(function(){
 
     // 2. build scene
     var ourScene = new ScrollMagic.Scene({
-        triggerElement: '#project01'
+        triggerElement: '#project01 img',
+        // duration: 300, // 300 pixels
+        duration: '90%', // responsive, 100% of view port height
+        triggerHook: 0.9
     })
         .setClassToggle('#project01', 'fade-in') // add class to project01
         .addIndicators({
             name: 'fade scene',
             colorTrigger: 'black',
-            indent: 200,
-            colorStart: 'lightgreen'
+            // indent: 200,
+            colorStart: 'lightgreen',
+            colorEnd: 'pink'
         }) // requires debug.addIndicators.min.js plugin
         .addTo(controller);
 
