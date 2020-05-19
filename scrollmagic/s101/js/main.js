07a432f46767ae30509ad4d5d384bce7e2bf9afd
Sanjib Ahmad <sanjib@sanjib.org>
2020-05-20 03:02:54 +0800

    Add scroll magic 101 pinning

--- scrollmagic/s101/js/main.js
+++ scrollmagic/s101/js/main.js
@@ -1,25 +1,22 @@
 $(document).ready(function(){
 
-	// 1. init scrollmagic
+	// 1. init scroll magic
     var controller = new ScrollMagic.Controller();
 
-    // // 2. build scene
-    // var ourScene = new ScrollMagic.Scene({
-    //     triggerElement: '#project01 img',
-    //     // duration: 300, // 300 pixels
-    //     // duration: '90%', // responsive, 100% of view port height
-    //     triggerHook: 0.9,
-    //     // reverse: false
-    // })
-    //     .setClassToggle('#project01', 'fade-in') // add class to project01
-    //     .addIndicators({
-    //         name: 'fade scene',
-    //         colorTrigger: 'black',
-    //         // indent: 200,
-    //         colorStart: 'lightgreen',
-    //         colorEnd: 'pink'
-    //     }) // requires debug.addIndicators.min.js plugin
-    //     .addTo(controller);
+    // pin the intro
+    var pinIntroScene = new ScrollMagic.Scene({
+        triggerElement: '#intro',
+        triggerHook: 0,
+        duration: '30%'
+    }).setPin('#intro', {pushFollowers: false})
+      .addTo(controller);
+
+    var pinIntroScene2 = new ScrollMagic.Scene({
+        triggerElement: '#project01',
+        triggerHook: 0,
+        duration: 0.4
+    }).setPin('#intro', {pushFollowers: false})
+        .addTo(controller);
 
     // loop through each .project element
     $('.project').each(function () {
@@ -30,16 +27,15 @@ $(document).ready(function(){
             // duration: '90%', // responsive, 100% of view port height
             triggerHook: 0.9,
             // reverse: false
-        })
-            .setClassToggle(this, 'fade-in') // add class to project01
-            .addIndicators({
-                name: 'fade scene',
-                colorTrigger: 'black',
-                // indent: 200,
-                colorStart: 'lightgreen',
-                colorEnd: 'pink'
-            }) // requires debug.addIndicators.min.js plugin
-            .addTo(controller);
+        }).setClassToggle(this, 'fade-in') // add class to project01
+          .addIndicators({
+              name: 'fade scene',
+              colorTrigger: 'black',
+              // indent: 200,
+              colorStart: 'lightgreen',
+              colorEnd: 'pink'
+          }) // requires debug.addIndicators.min.js plugin
+          .addTo(controller);
     });
 
 });
