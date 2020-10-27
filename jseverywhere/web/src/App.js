58ba5b24812f222f369d200107ce6d1af3550ef3
Sanjib Ahmad <sanjib@sanjib.org>
2020-10-28 03:16:07 +0800

    Init React app oak-notes-web (jse)

---
+++ jseverywhere/web/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Oak Notes</h1>
+      <p>Welcome to Oak Notes (jse)!</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
