d2fd074ccef3e9361bfcab4ca3322398ba9df337
Sanjib Ahmad <sanjib@sanjib.org>
2020-12-12 12:40:54 +0800

    Add programming with node project

---
+++ get_programming_with_node/ch02/messages.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let messages = [
+  "Act on your highest excitment",
+  "To the best of your ability",
+  "With zero insistence on outcome",
+]
---
+++ get_programming_with_node/ch02/positiveMessages.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let messages = [
+  "Act on your highest excitment",
+  "To the best of your ability",
+  "With zero insistence on outcome",
+]
+
+messages.forEach(message => console.log(message))
